0bc839bf30267780130545442f8d6a68ebf50b88
jackS <jastajas@gamil.com>
2018-02-11 13:19:15 +0100

    Klasa Prodact - ver.1

---
+++ src/Product.java
@@ -0,0 +1,28 @@
+public class Product {
+    private String name;
+    private String producer;
+    private double price;
+
+    public Product(String name, String producer, double price) {
+        this.name = name;
+        this.producer = producer;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getProducer() {
+        return producer;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    @Override
+    public String toString() {
+        return name + ";" + producer + ";" + price;
+    }
+}
